44e40e02edc4e8b79914cbdf79c2a2e71b5c2439
Dyesse YUMBA <yumba.nduba@gmail.com>
2017-12-22 12:35:14 +0100

    Updated components.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,10 +10,15 @@ import { select } from '@angular-redux/store';
 export class UserComponent implements OnInit {
   u: User[];
   @select('users') users$: Observable<User[]>;
-  @select(['performance', 'performancesA']) performancesA$: Observable<Performance[]>;
-  @select(['performance', 'performancesB']) performancesB$: Observable<Performance[]>;
+  @select(['performance', 'performancesA'])
+  performancesA$: Observable<Performance[]>;
+  @select(['performance', 'performancesB'])
+  performancesB$: Observable<Performance[]>;
 
-  constructor(private userActions: UserActions, private performanceAcions: PerformanceActions) {
+  constructor(
+    private userActions: UserActions,
+    private performanceAcions: PerformanceActions
+  ) {
     userActions.loadUsers();
     performanceAcions.loadPerformances();
   }
@@ -28,42 +33,31 @@ export class UserComponent implements OnInit {
     this.userActions.saveUsers(this.u);
   }
 
-  // onChange(value, type: string, index: number, index2: number) {
-  //   switch (type) {
-  //     case 'name':
-  //     this.u[index].user.name = value;
-  //     break;
+  onChange(value, type: string, index: number) {
+    switch (type) {
+      case 'name':
+        this.u[index].user.name = value;
+        break;
 
-  //     case 'radio':
-  //     this.handleRadion(value, index);
-  //     break;
+      case 'radio':
+      this.u[index].performancesA = [value];
+      break;
 
-  //     case 'checkbox':
-  //     this.u[index].performancesB[index2].name = value ? '1' : '0';
-  //     break;
-  //   }
-  // }
-
-  // private handleRadion (value: string, index: number) {
-  //   switch (value) {
-  //     case 'x': {
-  //       this.u[index].performancesA[0].name = '1';
-  //       this.u[index].performancesA[1].name = '0';
-  //       this.u[index].performancesA[2].name = '0';
-  //       break;
-  //     }
-  //     case 'y': {
-  //       this.u[index].performancesA[0].name = '0';
-  //       this.u[index].performancesA[1].name = '1';
-  //       this.u[index].performancesA[2].name = '0';
-  //       break;
-  //     }
-  //     case 'z': {
-  //       this.u[index].performancesA[0].name = '';
-  //       this.u[index].performancesA[1].name = '0';
-  //       this.u[index].performancesA[2].name = '1';
-  //       break;
-  //     }
-  //   }
-  // }
+      case 'checkbox':
+        const b = this.u[index].performancesB;
+        if (value.checked) {
+          if (!b.includes(value.value)) {
+            b.push(value.value);
+          }
+        } else {
+          if (b.includes(value.value)) {
+            const i = b.indexOf(value.value);
+            if (i !== -1) {
+              b.splice(i, 1);
+            }
+          }
+        }
+        break;
+    }
+  }
 }
